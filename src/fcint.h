cf9a172f7193923e0ffa4d2c35f0c51b6a656a93
Mingye Wang <arthur200126@gmail.com>
2018-02-16 13:15:11 -0500

    Add bitset debug printing
    This commit moves FcBitset's definition to fcint.h and adds debug
    printing support. Such a function can be used for verbose output.

--- src/fcint.h
+++ src/fcint.h
@@ -627,7 +627,11 @@ struct _FcValuePromotionBuffer {
 
 /* fcbitset.c */
 
-typedef struct _FcBitset FcBitset;
+typedef struct _FcBitset {
+    size_t size;
+    size_t ones;
+    FcChar8 data[];
+} FcBitset;
 
 FcPrivate FcBitset *
 FcBitsetCreate (size_t size);
@@ -913,6 +917,9 @@ FcCharSetPrint (const FcCharSet *c);
 
 FcPrivate void
 FcPatternPrint2 (FcPattern *p1, FcPattern *p2, const FcObjectSet *os);
+
+FcPrivate void
+FcBitsetPrint (const FcBitset *s);
 
 extern FcPrivate int FcDebugVal;
 
